cce2c3d93857797697dfe86504f916977068e382
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-21 11:35:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const parseCode = (str) => {
-  // your code here
+ const parseCode = (str) => {
+  const res = { 
+    firstName: "",
+    lastName: "",
+    id: ""
+  };
+  
+  let currentProp = 'firstName';
+
+  for (let item of str) {
+    if (item === '0') {
+      if (currentProp === 'firstName') {
+        currentProp = 'lastName';
+      } else if (currentProp === 'lastName') {
+        currentProp = 'id';
+      }
+    } else {
+      res[currentProp] += item;
+    }
+  }
+
+  return res;
 };
 
-// Do not change the code below
+// Example usage.
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
